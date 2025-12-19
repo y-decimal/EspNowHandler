3cc6e3f43ed9666b9b61d31b0e0b433e98bade16
y-decimal <83334510+y-decimal@users.noreply.github.com>
2025-12-19 15:59:01 +0100

    feat: copy DeviceRegistry from existing project

---
+++ include/DeviceRegistry.h
@@ -0,0 +1,43 @@
+#ifndef DEVICEREGISTRY_H
+#define DEVICEREGISTRY_H
+
+#include <array>
+#include <Preferences.h>
+
+using MacArray = std::array<uint8_t, 6>;
+
+class DeviceRegistry
+{
+
+public:
+    DeviceRegistry();
+
+    const uint8_t *getMac(uint8_t id) const;
+    const MacArray getRawMac(uint8_t id) const;
+
+    const uint8_t getIdFromMac(const uint8_t *macPtr) const;
+    const uint8_t getIdFromMac(MacArray &targetMac) const;
+
+    void setMac(uint8_t id, const uint8_t *macPtr);
+    void setMac(uint8_t id, const std::array<uint8_t, 6> &mac);
+
+    void saveToFlash();
+
+private:
+    struct DeviceEntry
+    {
+        MacArray mac{0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+        bool active = false;
+    };
+
+    std::array<DeviceEntry, sizeof(uint8_t)> table{};
+
+    Preferences prefs;
+
+    static constexpr const char *REGISTRY_NAMESPACE = "dReg";
+    static constexpr const char *REGISTRY_KEY = "val";
+
+    void readFromFlash();
+};
+
+#endif
---
+++ src/DeviceRegistry.cpp
@@ -0,0 +1,62 @@
+#include <DeviceRegistry.h>
+
+DeviceRegistry::DeviceRegistry()
+{
+    DeviceRegistry::readFromFlash();
+}
+
+const uint8_t *DeviceRegistry::getMac(uint8_t id) const
+{
+    return table[id].mac.data();
+}
+
+const std::array<uint8_t, 6> DeviceRegistry::getRawMac(uint8_t id) const
+{
+    return table[id].mac;
+}
+
+const uint8_t DeviceRegistry::getIdFromMac(const uint8_t *macPtr) const
+{
+    for (uint8_t i = 0; i < sizeof(table); ++i)
+    {
+        if (memcmp(table[i].mac.data(), macPtr, 6) == 0)
+            return i;
+    }
+    return 0xFF;
+}
+
+const uint8_t DeviceRegistry::getIdFromMac(std::array<uint8_t, 6> &targetMac) const
+{
+    for (uint8_t i = 0; i < sizeof(table); ++i)
+    {
+        if (table[i].mac == targetMac)
+            return i;
+    }
+    return 0xFF;
+}
+
+void DeviceRegistry::setMac(uint8_t id, const uint8_t *macPtr)
+{
+    memcpy(table[id].mac.data(), macPtr, 6);
+    table[id].active = true;
+}
+
+void DeviceRegistry::setMac(uint8_t id, const std::array<uint8_t, 6> &mac)
+{
+    table[id].mac = mac;
+    table[id].active = true;
+}
+
+void DeviceRegistry::saveToFlash()
+{
+    prefs.begin(REGISTRY_NAMESPACE, false);
+    prefs.putBytes(REGISTRY_KEY, (const uint8_t *)&table, sizeof(table));
+    prefs.end();
+}
+
+void DeviceRegistry::readFromFlash()
+{
+    prefs.begin(REGISTRY_NAMESPACE, false);
+    prefs.getBytes(REGISTRY_KEY, (uint8_t *)&table, sizeof(table));
+    prefs.end();
+}
