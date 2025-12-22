067d9eb251ff5e230f5d85d459760d20ecbb7def
y-decimal <83334510+y-decimal@users.noreply.github.com>
2025-12-22 21:59:41 +0100

    feat: add constructor with selfDeviceName parameter

--- include/EspNowHandler.h
+++ include/EspNowHandler.h
@@ -11,6 +11,8 @@ private:
 
 
 public:
+    EspNowHandler(const char *selfDeviceName); // Initializes the class and registers the given name as the own device name (mainly used for pairing).
+
     uint8_t registerComms(const char *targetName, bool pairingMode = false); // Returns a 1-Byte integer ID for the target device to use as the commID for sending packets and for identifying where packets were sent from. If no device of the given name exists, and pairingMode is true, enters pairing mode to try and find the device
 
     bool registerCallback(uint8_t packetTypeID, PacketCallback); // Registers a callback function for a specific packet type. Multiple callbacks per type not possible. PacketCallback must be format "void function(const uint8_t dataPtr, size_t len, uint8_t sender)"
