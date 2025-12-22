ca3b5e6d16cf9e8c3e3f2e72050ef239fdeda01e
y-decimal <83334510+y-decimal@users.noreply.github.com>
2025-12-22 22:24:09 +0100

    refactor: update methods to utilize 1-Byte addresses instead of strings

--- include/EspNowHandler.h
+++ include/EspNowHandler.h
@@ -8,19 +8,19 @@ using PacketCallback = std::function<void(const uint8_t *dataPtr, size_t len, ui
 class EspNowHandler
 {
 private:
-    bool pairDevice(const char *targetName); // Pairs a specific device by sending broadcasts with the target device name and the sender device name, the receiving side checks if the device name matches its own name, and if it does saves the sender device name and MAC to its registry, disables discovery mode if active, then sends an identical packet to the sender specifically. The sender receives the answer, saves the device name and MAC to its registry, then disables discovery mode if active.
+    bool pairDevice(uint8_t targetDeviceID); // Pairs a specific device by sending broadcasts with the target device ID and the sender device ID, the receiving side checks if the device ID matches its own ID, and if it does saves the sender device ID and MAC to its registry, disables discovery mode if active, then sends an identical packet to the sender specifically. The sender receives the answer, saves the device ID and MAC to its registry, then disables discovery mode if active.
 
 public:
-    EspNowHandler(const char *selfDeviceName); // Initializes the class and registers the given name as the own device name (mainly used for pairing).
+    EspNowHandler(uint8_t selfDeviceID); // Initializes the class and registers the given ID as the own device ID (mainly used for pairing).
 
-    uint8_t registerComms(const char *targetName, bool pairingMode = false); // Returns a 1-Byte integer ID for the target device to use as the commID for sending packets and for identifying where packets were sent from. If no device of the given name exists, and pairingMode is true, enters pairing mode to try and find the device
+    bool registerComms(uint8_t targetDeviceID, bool pairingMode = false); // If no device of the given ID exists in the registry, and pairingMode is true, enters pairing mode to try and find the device
 
     bool registerCallback(uint8_t packetTypeID, PacketCallback); // Registers a callback function for a specific packet type. Multiple callbacks per type not possible. PacketCallback must be format "void function(const uint8_t dataPtr, size_t len, uint8_t sender)"
 
-    bool sendPacket(uint8_t commID,
+    bool sendPacket(uint8_t targetDeviceID,
                     uint8_t packetTypeID,
                     const uint8_t *dataPtr,
-                    size_t len); // Sends a packet of the type "packetType" to a device with the corresponding commID (as returned when calling registerComms)
+                    size_t len); // Sends a packet of the type "packetTypeID" to a device with the corresponding targetDeviceID
 };
 
 #endif
