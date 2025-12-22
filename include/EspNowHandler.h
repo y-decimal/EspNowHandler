87ddf22229430af596960a29f4ea2cb9aee84ef0
y-decimal <83334510+y-decimal@users.noreply.github.com>
2025-12-22 21:12:54 +0100

    feat: add methods for registering communications, callbacks, and sending packets

--- include/EspNowHandler.h
+++ include/EspNowHandler.h
@@ -5,7 +5,15 @@
 
 class EspNowHandler
 {
+public:
+    uint8_t registerComms(const char *targetName, bool pairingMode = false); // Returns a 1-Byte integer ID to use as the CommID for sending packets to a device. If no device of the given name exists, and pairingMode is true, enters pairing mode to try and find the device
 
+    bool registerPacketTypeCb(const char *packetType, void *callbackFunction); // Registers a callback function for a specific packet type. Multiple callbacks per type possible
+
+    bool sendPacket(uint8_t commID,
+                    const char *packetType,
+                    const uint8_t *dataPtr,
+                    size_t len); // Sends a packet of the type "packetType" to a device with the corresponding commID (as returned when calling registerComms)
 };
 
 #endif
