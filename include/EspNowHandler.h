57ee565f4087baa1203b17fde19facc6fe7f0312
y-decimal <83334510+y-decimal@users.noreply.github.com>
2025-12-22 21:47:45 +0100

    refactor: update EspNowHandler class to improve callback registration and packet sending methods

--- include/EspNowHandler.h
+++ include/EspNowHandler.h
@@ -3,15 +3,20 @@
 
 #include <DeviceRegistry.h>
 
+using PacketCallback = std::function<void(const uint8_t *dataPtr, size_t len, uint8_t sender)>;
+
 class EspNowHandler
 {
-public:
-    uint8_t registerComms(const char *targetName, bool pairingMode = false); // Returns a 1-Byte integer ID to use as the CommID for sending packets to a device. If no device of the given name exists, and pairingMode is true, enters pairing mode to try and find the device
+private: 
 
-    bool registerPacketTypeCb(const char *packetType, void *callbackFunction); // Registers a callback function for a specific packet type. Multiple callbacks per type possible
+
+public:
+    uint8_t registerComms(const char *targetName, bool pairingMode = false); // Returns a 1-Byte integer ID for the target device to use as the commID for sending packets and for identifying where packets were sent from. If no device of the given name exists, and pairingMode is true, enters pairing mode to try and find the device
+
+    bool registerCallback(uint8_t packetTypeID, PacketCallback); // Registers a callback function for a specific packet type. Multiple callbacks per type not possible. PacketCallback must be format "void function(const uint8_t dataPtr, size_t len, uint8_t sender)"
 
     bool sendPacket(uint8_t commID,
-                    const char *packetType,
+                    uint8_t packetTypeID,
                     const uint8_t *dataPtr,
                     size_t len); // Sends a packet of the type "packetType" to a device with the corresponding commID (as returned when calling registerComms)
 };
