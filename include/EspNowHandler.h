fcff9ce9aa970b3be3e290cf4175991004815836
y-decimal <83334510+y-decimal@users.noreply.github.com>
2025-12-23 19:31:12 +0100

    refactor: update class to follow template style and feat: implement begin method for initializing ESP-NOW and registering callbacks with ESP-NOW

--- include/EspNowHandler.h
+++ include/EspNowHandler.h
@@ -2,25 +2,91 @@
 #define ESPNOWHANDLER_H
 
 #include <DeviceRegistry.h>
+#include <esp_now.h>
 
 using PacketCallback = std::function<void(const uint8_t *dataPtr, size_t len, uint8_t sender)>;
 
+#define ESP_NOW_HANDLER_TEMPLATE template <typename DeviceID, typename PacketType, size_t DeviceCount, size_t PacketCount>
+
+ESP_NOW_HANDLER_TEMPLATE
 class EspNowHandler
 {
 private:
     bool pairDevice(const char *targetName); // Pairs a specific device by sending broadcasts with the target device name and the sender device name, the receiving side checks if the device name matches its own name, and if it does saves the sender device name and MAC to its registry, disables discovery mode if active, then sends an identical packet to the sender specifically. The sender receives the answer, saves the device name and MAC to its registry, then disables discovery mode if active.
 
+    static void onDataSent(const uint8_t *macAddrPtr, esp_now_send_status_t status);
+    static void onDataRecv(const uint8_t *macAddrPtr, const uint8_t *dataPtr, int data_len);
+
+    struct PacketHeader
+    {
+        PacketType type;
+        size_t len;
+        uint8_t checksum;
+    };
+
+    DeviceRegistry<DeviceID, DeviceCount> *registry;
+
 public:
-    EspNowHandler(const char *selfDeviceName); // Initializes the class and registers the given name as the own device name (mainly used for pairing).
+    EspNowHandler(DeviceID selfDeviceID); // Initializes the class and registers the given name as the own device name (mainly used for pairing).
 
-    uint8_t registerComms(const char *targetName, bool pairingMode = false); // Returns a 1-Byte integer ID for the target device to use as the commID for sending packets and for identifying where packets were sent from. If no device of the given name exists, and pairingMode is true, enters pairing mode to try and find the device
+    bool begin();
 
-    bool registerCallback(uint8_t packetTypeID, PacketCallback); // Registers a callback function for a specific packet type. Multiple callbacks per type not possible. PacketCallback must be format "void function(const uint8_t dataPtr, size_t len, uint8_t sender)"
+    bool registerComms(DeviceID targetID, bool pairingMode = false); // Returns a 1-Byte integer ID for the target device to use as the commID for sending packets and for identifying where packets were sent from. If no device of the given name exists, and pairingMode is true, enters pairing mode to try and find the device
 
-    bool sendPacket(uint8_t commID,
-                    uint8_t packetTypeID,
+    bool registerCallback(PacketType packetTypeID, PacketCallback); // Registers a callback function for a specific packet type. Multiple callbacks per type not possible. PacketCallback must be format "void function(const uint8_t dataPtr, size_t len, uint8_t sender)"
+
+    bool sendPacket(DeviceID targetID,
+                    PacketType packetTypeID,
                     const uint8_t *dataPtr,
                     size_t len); // Sends a packet of the type "packetType" to a device with the corresponding commID (as returned when calling registerComms)
 };
+
+ESP_NOW_HANDLER_TEMPLATE
+EspNowHandler<DeviceID, PacketType, DeviceCount, PacketCount>::
+    EspNowHandler(DeviceID selfDeviceID)
+{
+    registry = new DeviceRegistry<DeviceID, DeviceCount>();
+}
+
+ESP_NOW_HANDLER_TEMPLATE
+bool EspNowHandler<DeviceID, PacketType, DeviceCount, PacketCount>::
+    begin()
+{
+    if (esp_now_init() != ESP_OK)
+    {
+        return false;
+    }
+    esp_now_register_send_cb(onDataSent);
+    esp_now_register_recv_cb(onDataRecv);
+    return true;
+}
+
+ESP_NOW_HANDLER_TEMPLATE
+bool EspNowHandler<DeviceID, PacketType, DeviceCount, PacketCount>::
+    registerComms(DeviceID targetID, bool pairingMode = false)
+{
+    const uint8_t targetMac[6] = {};
+    memcpy(targetMac, registry->getDeviceMac(targetID), 6);
+    if (targetMac == nullptr)
+    {
+        return false; // implement pairing here later
+    }
+    esp_now_peer_info_t peerInfo = {};
+    memcpy(peerInfo.peer_addr, targetMac, 6);
+    peerInfo.channel = 0;
+    peerInfo.encrypt = false;
+    esp_err_t addPeerReturn = esp_now_add_peer(&peerInfo);
+    if (addPeerReturn != ESP_OK)
+    {
+        return false;
+    }
+    return true;
+}
+
+ESP_NOW_HANDLER_TEMPLATE
+bool EspNowHandler<DeviceID, PacketType, DeviceCount, PacketCount>::
+    registerCallback(PacketType packetTypeID, PacketCallback)
+{
+}
 
 #endif
